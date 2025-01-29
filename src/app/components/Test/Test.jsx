05dd9174da5cf58179f44f8d3d632127214cc150
simarjot <simarjot0032@gmail.com>
2025-01-29 21:23:08 +0530

    added the previous and next button

--- src/app/components/Test/Test.jsx
+++ src/app/components/Test/Test.jsx
@@ -2,6 +2,8 @@ import LessonHeader from "./LessonHeader";
 import CardComponet from "./CardComponent";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import LeftArror from "@/lib/icon/LeftArror";
+import RightArrow from "@/lib/icon/RightArrow";
 
 const Test = ({
   profileImage,
@@ -56,11 +58,105 @@ const Test = ({
     }
   }, [queSelected, selectedCategory, setSelectedCategory]);
 
+  const handlePrevious = () => {
+    const lesson = Lessons[selectedTab];
+    const categories = lesson.categories;
+
+    const currentCategoryIndex = categories.findIndex(
+      (category) => category.catid === selectedCategory.catid
+    );
+    const currentQuestionIndex = selectedCategory.questions.findIndex(
+      (q) => q.id === queSelected?.id
+    );
+    for (let i = currentQuestionIndex - 1; i >= 0; i--) {
+      if (!selectedCategory.questions[i].status) {
+        setQueSelected(selectedCategory.questions[i]);
+        return;
+      }
+    }
+    for (let j = currentCategoryIndex - 1; j >= 0; j--) {
+      const prevCategory = categories[j];
+      const prevUnanswered = prevCategory.questions
+        .slice()
+        .reverse()
+        .find((q) => !q.status);
+      if (prevUnanswered) {
+        setSelectedCategory(prevCategory);
+        setQueSelected(prevUnanswered);
+        return;
+      }
+    }
+  };
+
+  const handleNext = () => {
+    const lesson = Lessons[selectedTab];
+    const categories = lesson.categories;
+    const currentCategoryIndex = categories.findIndex(
+      (category) => category.catid === selectedCategory.catid
+    );
+    const currentQuestionIndex = selectedCategory.questions.findIndex(
+      (q) => q.id === queSelected?.id
+    );
+    for (
+      let i = currentQuestionIndex + 1;
+      i < selectedCategory.questions.length;
+      i++
+    ) {
+      if (!selectedCategory.questions[i].status) {
+        setQueSelected(selectedCategory.questions[i]);
+        return;
+      }
+    }
+    for (let j = currentCategoryIndex + 1; j < categories.length; j++) {
+      const nextCategory = categories[j];
+      const nextUnanswered = nextCategory.questions.find((q) => !q.status);
+
+      if (nextUnanswered) {
+        setSelectedCategory(nextCategory);
+        setQueSelected(nextUnanswered);
+        return;
+      }
+    }
+  };
+
   return (
     <div className="w-full">
       <div className="flex items-center">
         <LessonHeader title={lessonName} />
-        <span className="text-[18px] font-[390] text-[#666666]">
+        <span
+          className="text-[18px] font-[390] text-[#666666] flex"
+          style={{ gap: "10px" }}
+        >
+          <button
+            disabled={(() => {
+              const lesson = Lessons[selectedTab];
+              const categories = lesson.categories;
+              const currentCategoryIndex = categories.findIndex(
+                (category) => category.catid === selectedCategory.catid
+              );
+              const currentQuestionIndex = selectedCategory.questions.findIndex(
+                (q) => q.id === queSelected?.id
+              );
+              const previousQuestionsInCurrentCategory =
+                selectedCategory.questions.slice(0, currentQuestionIndex);
+              const hasUnansweredBefore =
+                previousQuestionsInCurrentCategory.some((q) => !q.status);
+              const previousCategories = categories.slice(
+                0,
+                currentCategoryIndex
+              );
+              const hasUnansweredInPreviousCategories = previousCategories
+                .flatMap((cat) => cat.questions)
+                .some((q) => !q.status);
+              return !(
+                hasUnansweredBefore || hasUnansweredInPreviousCategories
+              );
+            })()}
+            className="disabled:cursor-not-allowed"
+            onClick={handlePrevious}
+          >
+            <LeftArror />
+          </button>
           {selectedCategory.catid == 1
             ? queSelected?.id
             : queSelected.id +
@@ -72,11 +168,42 @@ const Test = ({
             (total, noofquestions) => total + noofquestions.questions.length,
             0
           )}
+          <button
+            disabled={(() => {
+              const lesson = Lessons[selectedTab];
+              const categories = lesson.categories;
+              const currentCategoryIndex = categories.findIndex(
+                (category) => category.catid === selectedCategory.catid
+              );
+              const currentQuestionIndex = selectedCategory.questions.findIndex(
+                (q) => q.id === queSelected?.id
+              );
+              const remainingQuestionsInCurrentCategory =
+                selectedCategory.questions.slice(currentQuestionIndex + 1);
+              const hasUnansweredAfter =
+                remainingQuestionsInCurrentCategory.some((q) => !q.status);
+
+              const followingCategories = categories.slice(
+                currentCategoryIndex + 1
+              );
+              const hasUnansweredInFollowingCategories = followingCategories
+                .flatMap((cat) => cat.questions)
+                .some((q) => !q.status);
+              return !(
+                hasUnansweredAfter || hasUnansweredInFollowingCategories
+              );
+            })()}
+            className="disabled:cursor-not-allowed"
+            onClick={handleNext}
+          >
+            <RightArrow />
+          </button>
         </span>
       </div>
       <div className="flex flex-col  md:mt-0 text-white rounded-lg w-[100%]">
         {queSelected ? (
           <CardComponet
+            key={queSelected?.id}
             imageSrc={profileImage}
             imageAlt="Profile Image"
             categoryId={selectedCategory.catid}
--- src/lib/icon/ProfileIcon.jsx
+++ src/lib/icon/ProfileIcon.jsx
@@ -1,16 +1,29 @@
 function ProfileIcon() {
-    return (
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-<rect width="24" height="24" rx="12" fill="url(#pattern0_8_1015)"/>
-<defs>
-<pattern id="pattern0_8_1015" patternContentUnits="objectBoundingBox" width="1" height="1">
-<use xlink:href="#image0_8_1015" transform="translate(-0.0224719) scale(0.00374532)"/>
-</pattern>
-</defs>
-</svg>
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+      <rect width="24" height="24" rx="12" fill="url(#pattern0_8_1015)" />
+      <defs>
+        <pattern
+          id="pattern0_8_1015"
+          patternContentUnits="objectBoundingBox"
+          width="1"
+          height="1"
+        >
+          <use
+            xlink:href="#image0_8_1015"
+            transform="translate(-0.0224719) scale(0.00374532)"
+          />
+        </pattern>
+      </defs>
+    </svg>
+  );
+}
 
-    );
-  }
-  
-  export default ProfileIcon;
-  
+export default ProfileIcon;
